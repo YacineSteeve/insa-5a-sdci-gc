30605def1ac01b17cad06522dd37b033eec6e2c1
Mateo Blyweert <mateo.bwe@gmail.com>
2026-01-19 16:29:02 +0100

    feat: new thresholds

--- src/main/java/com/blyweertboukari/sdci/managers/Knowledge.java
+++ src/main/java/com/blyweertboukari/sdci/managers/Knowledge.java
@@ -19,10 +19,10 @@ public class Knowledge {
 
     static final int MONITORING_INTERVAL_MS = 5000;
     static final int MOVING_WINDOW_SIZE = 20;
-    static final double GATEWAY_LATENCY_THRESHOLD = 20;
-    static final double GATEWAY_RPS_THRESHOLD = 20;
-    static final double SERVER_LATENCY_THRESHOLD = 20;
-    static final double SERVER_RPS_THRESHOLD = 20;
+    static final double GATEWAY_LATENCY_THRESHOLD = 500;
+    static final double GATEWAY_RPS_THRESHOLD = 4.5;
+    static final double SERVER_LATENCY_THRESHOLD = 250;
+    static final double SERVER_RPS_THRESHOLD = 4.5;
     static final int CPU_CHANGE_STEP_M = 10; // 10m
     static final int RAM_CHANGE_STEP_MI = 32; // 32Mi
 
